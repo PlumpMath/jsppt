85bcd65b4a38d86d4c62129b025b91df2c6859cb
Mikael Karlsson <karlsson.rm@gmail.com>
2017-03-16 13:48:26 +0100

    SimplePt.h: moved operations into declaration part

--- SimplePt.h
+++ SimplePt.h
@@ -10,94 +10,68 @@
 class StateProtothread : public Protothread
 {
 public:
-    unsigned int GetState();
+    unsigned int GetState() { return _ptLine; };
 };
-
-unsigned int StateProtothread::GetState(){
-    return _ptLine;
-}
-
 // -----------------------------------------------
 
 class ExpiryTimer
 {
 public:
-    void Start(unsigned long t);
-    bool Expired();
-    bool NextInterval();
+    void Start(unsigned long t){
+        _startTime = millis();
+        _expiryTime = t;
+    };
+
+    bool Expired(){
+        return (millis() - _startTime) >= _expiryTime;
+    };
+
+    bool NextInterval(){
+        if(Expired()){
+            _startTime += _expiryTime;
+            return true;
+        }else{
+            return false;
+        }
+    };
 
 private:
     unsigned long _expiryTime;
     unsigned long _startTime;
 };
 
-void ExpiryTimer::Start(unsigned long t)
-{
-    _startTime = millis();
-    _expiryTime = t;
-}
-
-bool ExpiryTimer::Expired()
-{
-    return (millis() - _startTime) >= _expiryTime;
-}
-
-bool ExpiryTimer::NextInterval()
-{
-    if(Expired()){
-        _startTime += _expiryTime;
-        return true;
-    }else{
-        return false;
-    }
-}
-
 // -----------------------------------------------
 template <class T, unsigned int N>
 class SimpleQueue
 {
 public:
-    SimpleQueue();
-    T Read();
-    bool Write(T item);
-    unsigned int Size();
-    unsigned int Count();
+    SimpleQueue() : head(0),tail(0),write_cnt(0) {};
+    T Read() {
+        if(write_cnt > 0){
+            int val = head;
+            head = (head + 1) % N;
+            write_cnt--;
+            return data[val];
+        }else{
+            return 0;
+        }
+    };
+
+    bool Write(T item){
+        if(write_cnt < N){
+            data[tail] = item;
+            tail = (tail + 1) % N;
+            write_cnt++;
+            return true;
+        }else{
+            return false;
+        }
+    };
+    unsigned int Size() { return N; };
+    unsigned int Count() { return write_cnt; };
 private:
     T data[N];
     unsigned int write_cnt, head, tail;
 };
-
-template <class T, unsigned int N>
-SimpleQueue<T, N>::SimpleQueue() : head(0),tail(0),write_cnt(0) {}
-
-template <class T, unsigned int N>
-bool SimpleQueue<T, N>::Write(T item) {
-    if(write_cnt < N){
-        data[tail] = item;
-        tail = (tail + 1) % N;
-        write_cnt++;
-        return true;
-    }else{
-        return false;
-    }
-}
-
-template <class T, unsigned int N>
-T SimpleQueue<T, N>::Read() {
-    if(write_cnt > 0){
-        int val = head;
-        head = (head + 1) % N;
-        write_cnt--;
-        return data[val];
-    }else{
-        return 0;
-    }
-}
-
-template <class T, unsigned int N>
-unsigned int SimpleQueue<T,N>::Size() { return N; }
-
-template <class T, unsigned int N>
-unsigned int SimpleQueue<T,N>::Count() { return write_cnt; }
 
 #endif // __SIMPLEPT_H__
